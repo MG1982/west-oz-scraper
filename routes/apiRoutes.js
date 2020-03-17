652e0e5e60a8074a5e0e69b127aa0643ba7bd997
Mani <manigonzalez82@gmail.com>
2020-03-17 21:36:54 +0800

    Added comments to apiRoutes

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,14 +1,14 @@
 const db = require("../models");
 
 module.exports = function (app) {
-
+    // Save Article
     app.put("/articles/:id", function (req, res) {
         db.Article.update({ _id: req.params.id }, { saved: req.body.saved })
             .then(function (data) {
                 res.json(data);
             });
     });
-
+    // Add Comment to article
     app.post("/articles/:id", function (req, res) {
         db.Note.create(req.body)
             .then(function (dbNote) {
@@ -21,7 +21,7 @@ module.exports = function (app) {
                 res.json(err);
             });
     });
-
+    // Remove Comment from article
     app.delete("/notes/:id", function (req, res) {
         db.Note.deleteOne({ _id: req.params.id })
             .then(function (dbNotes) {
